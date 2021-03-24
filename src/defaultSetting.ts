01e739a5cdad909f8f21dc99901026605145d3e0
wangsheng <1355118962@qq.com>
2021-03-24 16:30:35 +0800

    fix: test git hooks

--- src/defaultSetting.ts
+++ src/defaultSetting.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: sheng.wang
  * @Date: 2021-03-18 15:19:19
- * @LastEditTime: 2021-03-18 15:20:17
+ * @LastEditTime: 2021-03-24 16:29:27
  * @LastEditors: sheng.wang
  * @Description: 
  * @FilePath: /react-cli/src/defaultSetting.ts
