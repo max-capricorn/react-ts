bf073a87db0b3885a337259414d9c1c5d469e25e
wangsheng <1355118962@qq.com>
2021-03-24 16:38:14 +0800

    fix: test git hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /*
  * @Author: sheng.wang
  * @Date: 2021-03-24 15:36:50
- * @LastEditTime: 2021-03-24 15:58:57
+ * @LastEditTime: 2021-03-24 16:38:10
  * @LastEditors: sheng.wang
  * @Description:
  * @FilePath: /react-cli/.eslintrc.js
@@ -10,14 +10,22 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
   parser: ['@swc/core', '@typescript-eslint/parser'],
-  exclude: ['plugin:@typescript-eslint/recommended', "react-app", 'plugin：prettier/recommended' ，],
-  plugins: ["@typescript-eslint", "react"],
+  exclude: [
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'plugin：prettier/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'react'],
   parserOptions: {
     ecmaVersion: '@next',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true
+    }
   },
   rules: {}
 }
